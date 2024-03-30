37cabc267306078f15c9327c4db48ba9752762e1
seniorroland1 <rolandantwisenior47@gmail.com>
2024-03-30 00:25:24 +0000

    add cartlist

---
+++ src/app/(user)/menu/[id].tsx
@@ -0,0 +1,97 @@
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { Image, Text, View, Pressable, StyleSheet } from "react-native";
+import products from "@/assets/data/products";
+import { useState } from "react";
+import { defaultProductImage } from "@/src/components/ProductListItem";
+import Button from "@/src/components/Button";
+import { useCart } from "@/src/provider/CartProvider";
+import { CartItem, PizzaSize } from "@/src/types";
+
+const size: PizzaSize[] = ["S", "M", "L", "XL"];
+
+const productDetailScreen = () => {
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
+  const { addItem } = useCart();
+  const router = useRouter();
+
+  const { id } = useLocalSearchParams();
+  const product = products.find((product) => product.id.toString() === id);
+
+  if (!product) {
+    return <Text>Product not found</Text>;
+  }
+
+  const addToCart = () => {
+    addItem(product, selectedSize);
+    router.push("/cart");
+  };
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultProductImage }}
+      />
+
+      <Text>Select size</Text>
+      <View style={styles.sizes}>
+        {size.map((s) => (
+          <Pressable
+            onPress={() => setSelectedSize(s)}
+            key={s}
+            style={[
+              styles.size,
+              {
+                backgroundColor: selectedSize === s ? "gainsboro" : "white",
+              },
+            ]}
+          >
+            <Text
+              style={[
+                styles.sizeText,
+                { color: selectedSize ? "gray" : "white" },
+              ]}
+            >
+              {s}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <Text style={styles.price}>{product.price}</Text>
+      <Button onPress={addToCart} text="Add to cart" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: "auto",
+  },
+  sizes: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: "gainsboro",
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sizeText: { fontSize: 20, fontWeight: "500" },
+});
+export default productDetailScreen;
--- src/components/CartListItem.tsx
+++ src/components/CartListItem.tsx
@@ -1,12 +1,15 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import React from "react";
+import Colors from "../constants/Colors";
 import { CartItem } from "../types";
-import { defaultProductImage } from "./ProductListItem";
 import { FontAwesome } from "@expo/vector-icons";
 import { useCart } from "../provider/CartProvider";
+import { defaultProductImage } from "./ProductListItem";
 
 type CartListItemProps = {
   cartItem: CartItem;
 };
+
 const CartListItem = ({ cartItem }: CartListItemProps) => {
   const { updateQuantity } = useCart();
   return (
@@ -16,13 +19,10 @@ const CartListItem = ({ cartItem }: CartListItemProps) => {
         style={styles.image}
         resizeMode="contain"
       />
-
       <View style={{ flex: 1 }}>
         <Text style={styles.title}>{cartItem.product.name}</Text>
         <View style={styles.subtitleContainer}>
-          <Text style={styles.price}>
-            $ {cartItem.product.price.toFixed(2)}
-          </Text>
+          <Text style={styles.price}>${cartItem.product.price.toFixed(2)}</Text>
           <Text>Size: {cartItem.size}</Text>
         </View>
       </View>
@@ -33,6 +33,7 @@ const CartListItem = ({ cartItem }: CartListItemProps) => {
           color="gray"
           style={{ padding: 5 }}
         />
+
         <Text style={styles.quantity}>{cartItem.quantity}</Text>
         <FontAwesome
           onPress={() => updateQuantity(cartItem.id, 1)}
@@ -44,6 +45,7 @@ const CartListItem = ({ cartItem }: CartListItemProps) => {
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
@@ -54,14 +56,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    width: "100%",
+    width: 75,
     aspectRatio: 1,
+    alignSelf: "center",
+    marginRight: 10,
   },
-  title: {},
-  subtitleContainer: {},
-  price: {},
-  size: {},
-  quantitySelector: {},
-  quantity: {},
+  title: {
+    fontWeight: "500",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  subtitleContainer: {
+    flexDirection: "row",
+    gap: 5,
+  },
+  quantitySelector: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  quantity: {
+    fontWeight: "500",
+    fontSize: 18,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
 });
+
 export default CartListItem;
